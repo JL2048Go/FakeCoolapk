f1dd2c6ae92f4f07b3a81da27c39497eaedc0ec7
liangjialin <JL2048@outlook.com>
2025-07-17 10:28:47 +0800

    test2

--- app/src/main/java/com/jl2048/fakecoolapk/MainActivity.kt
+++ app/src/main/java/com/jl2048/fakecoolapk/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
 
                         //test1
                         //test main
+                        //test2
                     )
                 }
             }
